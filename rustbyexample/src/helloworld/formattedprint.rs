8008e38fdcd62d624ba8665b8d307651cb25065c
takatori <takatorisatoshi@team-lab.com>
2016-11-03 17:53:19 +0900

    Add rust by example

---
+++ rustbyexample/src/helloworld/formattedprint.rs
@@ -0,0 +1,28 @@
+///
+///
+/// ``format!`` : Stringにフォーマットされたテキストを出力する
+/// ``print!``  : format!と同じだが、コンソールに出力する
+/// ``println!``: print!と同じだが、改行する
+fn main() {
+    
+    println!("{} days", 31);
+
+    // 位置指定できる
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+    // 名前付き引数もできる
+    println!("{subject} {verb} {object}",
+             object="the lazy dog",
+             subject="the quick brown fox",
+             verb="jumps over");
+
+    // ':'に続く文字で特別なフォーマットができる。 :b -> binary
+    // @see https://doc.rust-lang.org/beta/std/fmt/
+    println!("{} of {:b} know binary, the other half don't", 1, 2);
+
+    // 横幅を指定して右詰めできる
+    println!("{number:>width$}", number=1, width=6);
+
+    // ゼロパディング
+    println!("{number:0width$}", number=1, width=6);
+}
