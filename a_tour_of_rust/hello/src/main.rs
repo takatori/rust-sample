fff09446e3376fac2e9e456329cdcceccf1cb026
takatori <takatorisatoshi@team-lab.com>
2017-03-11 16:55:32 +0900

    Handling command line arguments

--- a_tour_of_rust/hello/src/main.rs
+++ a_tour_of_rust/hello/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+use std::str::FromStr;
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
@@ -13,7 +15,23 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = Vec::new();
+
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Uasge: gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The gratest common divisor of {:?} is {}", numbers, d);
 }
 
 
