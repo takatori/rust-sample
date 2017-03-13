68203d8c4d8f15f1f607bc8d9622d78fb6ea731a
takatori <takatorisatoshi@team-lab.com>
2017-03-13 23:51:34 +0900

    Add router

--- a_tour_of_rust/iron-gcd/src/main.rs
+++ a_tour_of_rust/iron-gcd/src/main.rs
@@ -1,14 +1,21 @@
 extern crate iron;
+extern crate router;
+
 #[macro_use]
 extern crate mime;
 
 use iron::prelude::*;
 use iron::status;
-
+use router::Router;
 
 fn main() {
+    let mut router = Router::new();
+
+    router.get("/", get_form, "root");
+    router.post("/gcd", post_gcd, "gcd");
+
     println!("Serving on http://localhost:3000...");
-    Iron::new(get_form).http("localhost:3000").unwrap();
+    Iron::new(router).http("localhost:3000").unwrap();
 }
 
 #[allow(unused_variables)]
