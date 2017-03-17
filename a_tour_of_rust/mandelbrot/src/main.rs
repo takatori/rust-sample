cb1f618c72fedb28b7f02ae9df6ae00751959c50
takatori <takatorisatoshi@team-lab.com>
2017-03-17 23:50:29 +0900

    add complex_squred_add_loop

--- a_tour_of_rust/mandelbrot/src/main.rs
+++ a_tour_of_rust/mandelbrot/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+extern crate num;
+
+use num::Complex;
+
+#[allow(dead_code)]
+fn complex_square_add_loop(c: Complex<f64>) {
+    let mut z = Complex { re: 0.0, im 0.0 };
+    loop {
+        z = z * z + c;
+    }
 }
