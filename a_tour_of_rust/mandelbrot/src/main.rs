b0d54033a7f317997a6e7509cd1c491be534f070
takatori <takatorisatoshi@team-lab.com>
2017-03-19 14:04:32 +0900

    Change concurrent

--- a_tour_of_rust/mandelbrot/src/main.rs
+++ a_tour_of_rust/mandelbrot/src/main.rs
@@ -1,5 +1,6 @@
 extern crate num;
 extern crate image;
+extern crate crossbeam;
 
 use num::Complex;
 use image::ColorType;
@@ -136,7 +137,26 @@ fn main() {
     let lower_right = parse_pair(&args[4], ',').expect("error parsing lower right corner point");
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render(&mut pixels, bounds, upper_left, lower_right);
+    let threads = 8;
+    let band_rows = bounds.1 / threads + 1;
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(band_rows * bounds.0).collect();
+        crossbeam::scope(|scope| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = band_rows * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+
+                scope.spawn(move || {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        })
+    }
+
     write_bitmap(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
 
