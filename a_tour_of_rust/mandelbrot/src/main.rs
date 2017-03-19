68715781571df1b305a07afa02e8207d7f6ecd8f
takatori <takatorisatoshi@team-lab.com>
2017-03-19 13:21:38 +0900

    Add render

--- a_tour_of_rust/mandelbrot/src/main.rs
+++ a_tour_of_rust/mandelbrot/src/main.rs
@@ -1,7 +1,11 @@
 extern crate num;
+extern crate image;
 
 use num::Complex;
+use image::ColorType;
+use image::png::PNGEncoder;
 use std::str::FromStr;
+use std::fs::File;
 
 #[allow(dead_code)]
 fn complex_square_add_loop(c: Complex<f64>) {
@@ -19,7 +23,7 @@ fn complex_square_add_loop(c: Complex<f64>) {
 /// origin. If `c` seems to be a member (more precisely, If we reached the
 /// iteration limit without being able to prove that `c` is not a member),
 /// return `None`
-fn excapes(c: Complex<f64>, limit: u32) -> Option<u32> {
+fn escapes(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
         z = z * z + c;
@@ -66,10 +70,50 @@ fn pixel_to_point(bounds: (usize, usize),
     // It might be nicer to find the position of the *middle* of the pixel,
     // instead of its upper left corner, but this is easier to write tests for.
     let (width, height) = (lower_right.0 - upper_left.0, upper_left.1 - lower_right.1);
+
     (upper_left.0 + pixel.0 as f64 * width / bounds.0 as f64,
-     upper_left.1 + pixel.1 as f64 * height / bounds.1 as f64)
+     upper_left.1 - pixel.1 as f64 * height / bounds.1 as f64)
 }
 
+/// Render a rectangle of the Mandelbrot set into a buffer of pixels.
+/// The `bounds` argument gives the width and height of the buffer `pixels`,
+/// which holds one grayscale piexel per byte. The `upper_left` and `lower_right`
+/// arguments specify points on the complex
+fn render(pixels: &mut [u8],
+          bounds: (usize, usize),
+          upper_left: (f64, f64),
+          lower_right: (f64, f64)) {
+
+    assert!(pixel.len() == bounds.0 * bounds.1);
+
+    for r in 0..bounds.1 {
+        for c in 0..bounds.0 {
+
+            let point = pixel_to_point(bounds, (c, r), upper_left, lower_right);
+
+            pixels[r * bounds.0 + c] = match escapes(Complex { re: point.0, im: point.1 }, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
+        }
+    }
+}
+
+/// Write the buffer `pixels`, whose dimensions are given by `bounds`, to the
+/// file name `filename`.
+fn write_bitmap(filename: &str,
+                pixels: &[u8],
+                bounds: (usize, usize))
+                -> Result<(), std::io::Error> {
+
+    let output = File::create(filename)?;
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels,
+                bounds.0 as u32,
+                bounds.1 as u32,
+                ColorType::Gray(8))?;
+    Ok(())
+}
 
 #[test]
 fn test_parse_pair() {
